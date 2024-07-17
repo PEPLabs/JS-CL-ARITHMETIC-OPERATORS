784216aacb7250b6b940753be8cbe8de21f5a069
Revathi-Kamineni <104614509+Revathi-Kamineni@users.noreply.github.com>
2024-07-17 14:33:22 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.openqa.selenium.JavascriptExecutor;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumTest {
